c2eeb28264044301fdc37c4d008aee575956dd34
amankrm-dev <shoorveer681@gmail.com>
2025-03-28 21:32:19 +0545

    Higher or Lower Game

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,12 +1,27 @@
-def my_function():
-    for i in range(1, 20):
-        if i == 20:
-            print("You got it")
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
+# my_function()
 
 
-my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# The for loop is traversing from index 1 to index 19 in the above code as range (1,20) means that the for loop should move from 1 to (20-1)
+
 # 2. When is the function meant to print "You got it"?
+#     While traversing from start to the end if the value of i is equals to 20 then the if condition satisfies
+
 # 3. What are your assumptions about the value of i?
+#  If we make the range function (1, 20+1) or simply (1,21) we will get the value of i as 20
+
+
+
+# Debugging
+
+def my_function():
+    for i in range(1, 20+1):
+        if i == 20:
+            print("You got it")
+my_function()
--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,6 @@
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
+print(f"Pages={pages} and Words Per Page={word_per_page}")
 print(total_words)
