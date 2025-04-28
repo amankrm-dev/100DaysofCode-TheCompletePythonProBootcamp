d2ce5e5316358d66cebe142ab9623170f5187d94
amankrm-dev <shoorveer681@gmail.com>
2025-04-28 19:28:00 +0545

    Colorgram Project Using Tutrle

--- Day 18/shapes.py
+++ Day 18/shapes.py
@@ -1,22 +1,35 @@
+import random
 import turtle
 from turtle import Turtle
 tim=Turtle()
+turtle.colormode(255)
+def random_colour():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    random_color=(r,g,b)
+    return random_color
 tim.shape("turtle")
 # corners=3
-colors=["red","green","tomato","yellow","skyblue","pink","blue"]
+# colors=["red","green","tomato","yellow","skyblue","pink","blue"]
+directions=[0,90,180,270]
+tim.pensize(15)
+tim.speed("fastest")
+# def draw(corners,color_index):
+#     for i in range(corners):
+#         deg = 360 / corners
+#         tim.pencolor(colors[color_index % len(colors)])
+#         tim.forward(100)
+#         tim.right(deg)
+#     corners+=1
+#
+#
+# for shape_sides in range(3,11):
+#     draw(shape_sides,shape_sides-3)
 
-def draw(corners,color_index):
-    for i in range(corners):
-        deg = 360 / corners
-        tim.pencolor(colors[color_index % len(colors)])
-        tim.forward(100)
-        tim.right(deg)
-    corners+=1
-
-
-for shape_sides in range(3,11):
-    draw(shape_sides,shape_sides-3)
-
-
-
+for _ in range(200):
+    # tim.color(random.choice(colors))
+    tim.color(random_colour())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 turtle.exitonclick()
